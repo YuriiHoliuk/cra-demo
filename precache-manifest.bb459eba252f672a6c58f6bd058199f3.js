08c85f179940e1229551e4335044829f675d8351
Yurii Holiuk <bionicuss@gmail.com>
2020-02-12 10:03:35 +0200

    Updates

---
+++ precache-manifest.bb459eba252f672a6c58f6bd058199f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff61df331c645caf487069463f8087a",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "671757cf77c5959b7cd4",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e7d5fa7a5b2bd321d6",
+    "url": "/cra-demo/static/js/main.b9f42bd7.chunk.js"
+  },
+  {
+    "revision": "107a7a11fab40b66012c",
+    "url": "/cra-demo/static/js/runtime-main.b7e94f1e.js"
+  }
+]);
